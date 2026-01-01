3d1d145b11b4e29680e9b0efb2dd9de6642ef198
knightfury16 <suhaibknight@gmail.com>
2026-01-01 20:29:36 +0600

    Image viewer init commit

---
+++ ImageViewer/Program.cs
@@ -0,0 +1,21 @@
+﻿using Silk.NET.Windowing;
+
+namespace ImageViewer;
+
+class Program
+{
+    private static IWindow? _window;
+
+    static void Main(string[] args)
+    {
+        WindowOptions options = WindowOptions.Default with
+        {
+            Size = new Silk.NET.Maths.Vector2D<int>(800, 600),
+            Title = "Image Viewer"
+        };
+
+        _window = Window.Create(options);
+
+        _window.Run();
+    }
+}
